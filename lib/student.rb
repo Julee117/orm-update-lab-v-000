67e7443971512ee0bbe1044d4fce726e894c0253
Julianne Kim <juliannne@gmail.com>
2017-11-06 23:11:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,13 +60,12 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
+      LIMIT 1
     SQL
 
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
-    # DB[:conn].execute(sql, name).map do |row|
-    #   self.new_from_db(row)
-    # end
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
